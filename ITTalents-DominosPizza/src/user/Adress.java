db73ee914d6115010a87e3eb197861deb069ece1
IvanGospodinov <31139386+IvanGospodinov@users.noreply.github.com>
2017-09-15 18:22:43 +0300

    Add files via upload

---
+++ ITTalents-DominosPizza/src/user/Adress.java
@@ -0,0 +1,10 @@
+package user;
+
+public final class Adress {
+
+	private String street;
+	private String neighbourhood;
+	private String number;
+	private String postalCode
+	
+}
---
+++ ITTalents-DominosPizza/src/user/User.java
@@ -0,0 +1,12 @@
+package user;
+
+import java.util.Set;
+
+public class User {
+
+	private String firstName;
+	private String lastName;
+	private String password;
+	Set<Adress>
+	
+}
